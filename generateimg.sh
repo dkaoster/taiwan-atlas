b578d8fe7c5d2a69027f84c94841bf4d3c742567
Daniel Kao <dkao@diplateevo.com>
2020-12-05 17:35:35 +0800

    Tweak Images

--- generateimg.sh
+++ generateimg.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 topo2geo villages=- < villages-mercator-10t.json | geo2svg -o img/villages.svg -w 450 -h 600
-svgexport img/villages.svg img/villages.png 2x "path {stroke-width: 0.5px;}"
+svgexport img/villages.svg img/villages.png 2x "path {stroke-width: 0.2px;}"
 
 topo2geo towns=- < villages-mercator-10t.json | geo2svg -o img/towns.svg -w 450 -h 600
 svgexport img/towns.svg img/towns.png 2x "path {stroke-width: 0.5px;}"
 
 topo2geo counties=- < villages-mercator-10t.json | geo2svg -o img/counties.svg -w 450 -h 600
-svgexport img/counties.svg img/counties.png 2x "path {stroke-width: 0.5px;}"
+svgexport img/counties.svg img/counties.png 2x
 
 topo2geo nation=- < villages-mercator-10t.json | geo2svg -o img/nation.svg -w 450 -h 600
-svgexport img/nation.svg img/nation.png 2x "path {stroke-width: 0.5px;}"
+svgexport img/nation.svg img/nation.png 2x
